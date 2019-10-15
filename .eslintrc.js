1718b2b7c12c05285fe3c1f9f078ed3535858249
mihnsen <minhnt.hut@gmail.com>
2019-10-16 05:08:22 +0700

    add promise button

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
     }
-  ]
-}
+};
---
+++ src/components/PromiseButton.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+button.promise-button(
+  type="button",
+  @click.stop="handle",
+  :class="{ 'promise-button--notext': !isShowText }"
+)
+  span(v-if="isShowText")
+    slot
+      span.promise-button-text
+  slot(name="spinner")
+    span.promise-button-spinner(
+      v-show="isProcessing || forceProcessing",
+      role="status",
+    )
+      span.sr-only Loading...
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class PromiseButton extends Vue {
+  @Prop({ type: Function, required: true })
+  promise!: Function
+
+  @Prop({ type: Boolean, default: false })
+  autoHide!: boolean
+
+  @Prop({ type: Boolean, default: false })
+  forceProcessing!: boolean
+
+  isProcessing: boolean = false;
+
+  /**
+  * get computed of show text or not
+  */
+  get isShowText() {
+    if (this.isProcessing) {
+      return !this.autoHide;
+    }
+
+    return true;
+  }
+
+  /**
+  * main handle function
+  */
+  handle() {
+    if (!this.isProcessing) {
+      this.isProcessing = true;
+      this.promise()
+        .catch((e: Error) => { // eslint-disable-line
+          this.isProcessing = false;
+          throw e;
+        })
+        .then(() => {
+          this.isProcessing = false;
+        });
+    }
+  }
+};
+</script>
+<style lang="sass" scoped>
+@import '~bootstrap/scss/functions'
+@import '~bootstrap/scss/variables'
+@import '~bootstrap/scss/mixins'
+
+@import '~bootstrap/scss/utilities/screenreaders'
+@import '~bootstrap/scss/spinners'
+
+.promise-button
+  display: inline-flex
+
+.promise-button-spinner
+  @extend .spinner-border
+  width: 1em
+  height: 1em
+  border-width: .15em
+  margin-left: .5em
+
+</style>
