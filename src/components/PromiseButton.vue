cc4fcdc41f6c7d826f755c21419a8a254899e120
mihnsen <minhnt.hut@gmail.com>
2020-04-05 00:24:03 +0700

    Use css global

--- src/components/PromiseButton.vue
+++ src/components/PromiseButton.vue
@@ -59,7 +59,7 @@ export default class PromiseButton extends Vue {
   }
 };
 </script>
-<style lang="sass" scoped>
+<style lang="sass">
 @import '~bootstrap/scss/functions'
 @import '~bootstrap/scss/variables'
 @import '~bootstrap/scss/mixins'
